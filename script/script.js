8cac8d29127d01e8798ad7b8eccb858c7a59f90d
mamo34 <102875810+mamo34@users.noreply.github.com>
2025-04-28 10:57:07 +0200

    n

--- script/script.js
+++ script/script.js
@@ -1,6 +1,28 @@
-fetch('./data/vestiges') // requête vers le fichier JSON
+fetch('./data/vestiges.json') // requête vers le fichier JSON
   .then(response => response.json()) // convertir la réponse textuelle en JSON
   .then(data => {
     // traiter les données
     console.log(data);
   });
+
+  function setTrackList(data){
+    // récupération du template
+    let template = document.getElementById('trackCard');
+
+    // parcourir les chansons
+    for (let i = 0; i < data.length; i++) {
+        // faire un clone tu template
+        const clone = template.content.cloneNode(true);
+
+        let artists= getDisplayArtists(data[i].artists);
+
+        // remplir le clone
+        clone.querySelector('.card-title').textContent = data[i].name;
+        clone.querySelector('.card-text').textContent = artists;
+        clone.querySelector('.card-img-top').src = data[i].album.images[0].url;
+        clone.querySelector('.card-img-top').alt = data[i].name;
+
+        // ajouter le clone au DOM dans le conteneur
+        document.getElementById('trackList').appendChild(clone);
+    }
+}
